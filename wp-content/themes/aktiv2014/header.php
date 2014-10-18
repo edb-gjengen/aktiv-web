93053cd8d5937046d813b6d78f063297e21831ff
Nikolai R Kristiansen <nikolaik@gmail.com>
2014-10-18 14:25:25 +0200

    Removes FB JS and adds Google Analytics

--- wp-content/themes/aktiv2014/header.php
+++ wp-content/themes/aktiv2014/header.php
@@ -28,14 +28,16 @@
 </head>
 
 <body <?php neuf_body_class(); ?>>
-    <script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/nb_NO/all.js#xfbml=1&appId=220213643760";
-        fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
-    <div id="fb-root"></div>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-52914-18', 'auto');
+  ga('send', 'pageview');
+
+    </script>
     <header id="site-header">
         <div id="header-container">
             <div class="site-title">
